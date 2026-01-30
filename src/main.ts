0640a3e666e1e4fda7c1d21031ad481e87be447f
RevanDTH <davidkrammer21@gmail.com>
2026-01-30 22:58:53 +0100

    Added html skeleton

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,18 @@
-// Your app code starts here
-console.log('App loaded')
+//Elements
+let newGradeButton = document.getElementById("insertGradeButton") as HTMLButtonElement;
+
+
+//Functions
+function addGrade(name:string,weighting:number,value:number) {
+    let gradesTable = document.getElementById("gradesTable") as HTMLTableElement;
+    let newRow = gradesTable.insertRow(gradesTable.rows.length);
+
+    newRow.insertCell(0).innerHTML = name;
+    newRow.insertCell(1).innerHTML = weighting.toString();
+    newRow.insertCell(2).innerHTML = value.toString();
+}
+
+
+
+//Listeners
+newGradeButton.addEventListener("click", () => addGrade("test",1.0,2.2));
